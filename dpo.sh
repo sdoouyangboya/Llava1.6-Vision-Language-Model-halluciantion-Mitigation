b30a200fe0df91de59584e58d1383c411c674be3
Boya <boya.ouyang@esperantotech.com>
2024-08-15 14:00:10 -0500

    a

---
+++ dpo.sh
@@ -0,0 +1,39 @@
+!deepspeed --include localhost:0,1 ha_dpo/models/llava-v1_5/train_dpo.py \
+    --lora_enable False \
+    --deepspeed ha_dpo/models/llava-v1_5/scripts/zero3.json \
+    --model_name_or_path liuhaotian/llava-v1.5-7b \
+    --vg_path ha_dpo/data/vg \
+    --desc_data_path ha_dpo/data/hadpo/llava-v1.5/hadpo_llava-v1.5_desc_data.json \
+    --pope_data_path ha_dpo/data/hadpo/llava-v1.5/hadpo_llava-v1.5_pope_data.json \
+    --vision_tower model/clip-vit-large-patch14-336 \
+    --freeze_backbone False \
+    --tune_mm_mlp_adapte True \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ha_dpo/models/liuhaotian/llava-v1.5-7b-dpo-ulfinetune-llava\
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50 \
+    --save_total_limit 5\
+    --learning_rate 2e-6 \
+    --weight_decay 0. \
+    --warmup_steps 0 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name "llava-v1.5" \
+    --beta 0.1
