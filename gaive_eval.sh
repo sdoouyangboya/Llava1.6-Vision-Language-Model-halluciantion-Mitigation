96578c8f4a4311e62cc5cd7ce945940e5e61f5f4
Boya <boya.ouyang@esperantotech.com>
2024-08-15 17:54:44 -0500

    a

---
+++ gaive_eval.sh
@@ -0,0 +1,6 @@
+llava/eval/model_vqa.py \
+    --model-path /mnt/datascience3/Boya/bouyang/LLaVA-1.6-ft/llava-lrv-soma-1epoch-20k-10k\
+    --question-file evaluation_set_llava.json\
+    --image-folder VG_merge\
+    --answers-file answer/mnt/datascience3/Boya/bouyang/LLaVA-1.6-ft/llava-lrv-soma-1epoch-20k-10k_gaive.json\
+    --temperature 0 \
