4769c7835832817acb5d66c2a9f7d8d561a2eef5
Boya <boya.ouyang@esperantotech.com>
2024-08-15 13:42:41 -0500

    a

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-deepspeed llava/train/train_mem.py \
+deepspeed --include localhost:2 LLaVA-1.6-ft/llava/train/train_mem.py \
     --lora_enable True --lora_r 16 --lora_alpha 32 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero3.json \
-    --model_name_or_path liuhaotian/llava-v1.6-34b \
-    --version chatml_direct_ft \
-    --data_path transformed_data.json \
-    --image_folder train_images \
+    --deepspeed LLaVA-1.6-ft/scripts/zero3.json \
+    --model_name_or_path liuhaotian/llava-v1.6-mistral-7b \
+    --version mistral_instruct \
+    --data_path bouyang/llava-finetune-data/som_qa_coco20k.json \
+    --image_folder image \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -15,16 +13,16 @@ deepspeed llava/train/train_mem.py \
     --mm_patch_merge_type spatial_unpad \
     --image_aspect_ratio anyres \
     --group_by_modality_length False \
-    --bf16 True \
-    --fp16 False \
-    --output_dir ./llava-lora-34b \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
+    --bf16 False \
+    --fp16 True \
+    --output_dir mistral-llava \
+    --num_train_epochs 2 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 250 \
+    --save_steps 500 \
     --save_total_limit 5 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
@@ -36,4 +34,4 @@ deepspeed llava/train/train_mem.py \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb \
+    --report_to "wandb"
